655ffe65221d3bf4d828b8bd41becfeea59cae27
junmn <sfd7@naver.com>
2019-03-12 16:20:25 +1300

    Circles can move
    User can swap circles horizontally or vertically

--- Scripts/Circle.cs
+++ Scripts/Circle.cs
@@ -2,15 +2,111 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Circle : MonoBehaviour {
+public class Circle : MonoBehaviour  {
+    int x_position, y_position;
+    private Vector2 touch_position, release_position;
+    private float swipe_angle;
+    private GamePanel_Controller gamepanel;
 
 	// Use this for initialization
 	void Start () {
-		
+        gamepanel = FindObjectOfType<GamePanel_Controller>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if(transform.position.x != x_position || transform.position.y != y_position)
+        {
+            transform.position = Vector2.Lerp(transform.position, new Vector2(x_position, y_position),.4f);
+            //transform.position = new Vector2(x_position, y_position);
+
+            //Mathf.Lerp(transform.position.x,x_position, 0.4f);
+            //Mathf.Lerp(transform.position.y,y_position, 0.4f);
+        }
+
+        //transform.name = "( " + x_position + ", " + y_position + " )";
+
+    }
+    private void OnMouseDown()
+    {
+        touch_position = Camera.main.WorldToScreenPoint( Input.mousePosition);
+    }
+    private void OnMouseUp()
+    {
+        release_position = Camera.main.WorldToScreenPoint(Input.mousePosition);
+        swipe_angle= Mathf.Atan2((release_position.y - touch_position.y) , (release_position.x - touch_position.x))/Mathf.PI *180;
+
+        GameObject other_circle = null;
+        //Vector2 destination_point = this.transform.position;
+        if (swipe_angle<=45 && swipe_angle > -45 && x_position <gamepanel.width-1) // swipe right
+        {
+            other_circle = gamepanel.circles_on_panel[x_position + 1, y_position];
+            other_circle.GetComponent<Circle>().SetPosition(new Vector2(x_position, y_position));
+
+            //swapping circles in the array in the game panel
+            gamepanel.circles_on_panel[x_position + 1, y_position] = gamepanel.circles_on_panel[x_position, y_position];
+            gamepanel.circles_on_panel[x_position, y_position] = other_circle;
+            
+            this.x_position += 1;
+            
+
+
+            /*destination_point = other_circle.transform.position;
+            Mathf.Lerp(other_circle.transform.position.x, transform.position.x, 0.4f);
+            Mathf.Lerp(other_circle.transform.position.y, transform.position.y, 0.4f);
+
+            Mathf.Lerp(transform.position.x, destination_point.x, 0.4f);
+            Mathf.Lerp(transform.position.y, destination_point.y, 0.4f);*/
+
+        }
+        else if(swipe_angle <= -45 && swipe_angle > -135 && y_position>0) //swipe bottom
+        {
+            other_circle = gamepanel.circles_on_panel[x_position, y_position-1];
+            other_circle.GetComponent<Circle>().SetPosition(new Vector2(x_position, y_position));
+
+            //swapping circles in the array in the game panel
+            gamepanel.circles_on_panel[x_position, y_position-1] = gamepanel.circles_on_panel[x_position, y_position];
+            gamepanel.circles_on_panel[x_position, y_position] = other_circle;
+
+            this.y_position -= 1;
+        }
+        else if(swipe_angle >= Mathf.Abs(135) && x_position >0) //swipe left
+        {
+            other_circle = gamepanel.circles_on_panel[x_position - 1, y_position];
+            other_circle.GetComponent<Circle>().SetPosition(new Vector2(x_position, y_position));
+
+            //swapping circles in the array in the game panel
+            gamepanel.circles_on_panel[x_position - 1, y_position] = gamepanel.circles_on_panel[x_position, y_position];
+            gamepanel.circles_on_panel[x_position, y_position] = other_circle;
+
+            this.x_position -= 1;
+        }
+        else if(swipe_angle <135 && swipe_angle >45 && y_position < gamepanel.height) //swipe top
+        {
+
+
+            other_circle = gamepanel.circles_on_panel[x_position, y_position+1];
+
+            //swapping circles in the array in the game panel
+            gamepanel.circles_on_panel[x_position, y_position+1] = gamepanel.circles_on_panel[x_position, y_position];
+            gamepanel.circles_on_panel[x_position, y_position] = other_circle;
+
+            other_circle.GetComponent<Circle>().SetPosition(new Vector2(x_position, y_position));
+            this.y_position += 1;
+        }
+        
+    }
+
+
+
+
+    public void SetPosition(Vector2 pos)
+    {
+        this.x_position = (int)pos.x;
+        this.y_position = (int)pos.y;
+    }
+    public Vector2 GetPosition()
+    {
+        return new Vector2(x_position, y_position);
+    }
 }
--- Scripts/GamePanel_Controller.cs
+++ Scripts/GamePanel_Controller.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GamePanel_Controller : MonoBehaviour {
+    public int width, height;
+    public GameObject background_tile;
+    public GameObject[] circle_types;
+    public GameObject[,] circles_on_panel;
+    
+
 
 	// Use this for initialization
 	void Start () {
-		
+        Init();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+    public void Init()
+    {
+        circles_on_panel = new GameObject[width, height];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++)
+            {
+                GameObject temp_tile = GameObject.Instantiate(background_tile, new Vector2(j, i), Quaternion.identity) as GameObject;
+                temp_tile.transform.parent = this.transform;
+                temp_tile.name = "(" + j + " ," + i + ")";
+
+                GameObject random_type_circle = circle_types[Random.Range(0, circle_types.Length)];
+                GameObject new_circle = GameObject.Instantiate(random_type_circle, new Vector2(j, i), Quaternion.identity) as GameObject;
+                new_circle.GetComponent<Circle>().SetPosition(new Vector2(j, i));
+                circles_on_panel[j, i] = new_circle;
+                new_circle.transform.parent = this.transform;
+            }
+        }
+    }
 }
